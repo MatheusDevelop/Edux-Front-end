9bbaddef610b1ceff10e5f778005f38026fb04fb
ViniciusMirandaDev <vinimb07@gmail.com>
2020-10-30 23:55:51 -0300

    Gerenciador de dicas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { BrowserRouter,Switch,Route } from "react-router-dom";
 import GerenciadorTurmasPage from "./modules/gerenciadorTurmas/pages/gerenciadorTurmasPage";
 import loginPage from "./modules/login/pages/loginPage";
+import GerenciadorDicasPage from "./modules/gerenciadorDicas/pages/gerenciadorDicasPage"
 
 function Routes() {
     return(
         <BrowserRouter>
             <Switch>
+
                 <Route path="/login" component={loginPage}/>
                 <Route path="/gerenciadordeturmas" component={GerenciadorTurmasPage}/>
+                <Route path="/gerenciadordedicas" component={GerenciadorDicasPage}/>
+
             </Switch>
         </BrowserRouter>
     )
