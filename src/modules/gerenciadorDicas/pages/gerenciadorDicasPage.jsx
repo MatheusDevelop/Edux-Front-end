d71c30d437a7e9b5080453db1915ea2603f2d298
ViniciusMirandaDev <vinimb07@gmail.com>
2020-11-03 20:56:08 -0300

    Alterações

--- src/modules/gerenciadorDicas/pages/gerenciadorDicasPage.jsx
+++ src/modules/gerenciadorDicas/pages/gerenciadorDicasPage.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Footer from '../../../template/footer/footer';
 import Header from '../components/header';
-import {Row, Col ,NavDropdown, Container} from "react-bootstrap";
+import {Row, Col ,NavDropdown, Container, Card} from "react-bootstrap";
+import CrudDicas from '../components/crudDicas';
+import { url } from '../../../utils/constanturl';
+
+
 
 
 const GerenciadorDicasPage = () => {
+
+    const[dica,setDica] = useState([]);
+
+    useEffect(() => {
+        listar();
+    },[]);
+
+    const listar = () => {
+
+        fetch(`${url}/dica`)
+            .then(response => response.json())
+            .then(dados => {
+                setDica(dados.data);
+                
+                
+            })
+            .catch(err => console.error(err));
+    }
+
+
+    let list = dica.map((item)=>{ 
+               
+        return(
+            <Card descript={item.descricao} dica={item.idUsuarioNavigation.titulo}/>
+        )
+    })
 
     return (
         <div>
@@ -15,6 +45,16 @@ const GerenciadorDicasPage = () => {
                         Gerenciador<br />de
                         dicas
                </div>
+                </Row>
+                <Row>
+                    <Col>
+                        <CrudDicas setter ={setDica}/>
+                    </Col>
+                    <Col lg={7} className="d-flex align-items-center">
+                        <Row className="flex-wrap read__scroll">
+                            {list}
+                        </Row>
+                    </Col>
                 </Row>
                 
             </Container>
